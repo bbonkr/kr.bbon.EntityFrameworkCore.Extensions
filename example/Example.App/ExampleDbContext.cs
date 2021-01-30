3fd4144f635b72a87c5b5e2b52a3e608b90b3583
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2021-01-30 13:09:05 +0900

    Resolve #2 : Add Example project

---
+++ example/Example.App/ExampleDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Example.App
+{
+    public class ExampleDbContext : DbContext
+    {
+        public DbSet<Document> Documents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Example");
+        }
+    }
+}
