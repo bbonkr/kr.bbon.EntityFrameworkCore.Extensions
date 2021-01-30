ad920af1a756553172d99f9afc541a91ff751cc9
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2021-01-30 13:21:11 +0900

    Resolev #4 Support multiple sort

---
+++ example/Example.App/DatabaseHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Example.App
+{
+    public class DatabaseHelper :IDisposable
+    {
+      
+        public void InitializeDatabase()
+        {
+            using (var ctx = new ExampleDbContext())
+            {
+                ctx.Database.EnsureCreated();
+            }
+        }
+
+
+        public void Seed()
+        {
+            using (var ctx = new ExampleDbContext())
+            {
+                for (var id = 1; id <= 10; id++)
+                {
+                    ctx.Documents.Add(new Document
+                    {
+                        Id = id,
+                        Content = $"{(char)(65 + (id % 2))}",
+                        CreatedAt = DateTimeOffset.UtcNow.AddSeconds(id),
+                    });
+                }
+
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var ctx = new ExampleDbContext())
+            {
+                ctx.Database.EnsureDeleted();
+            }
+        }
+
+    }
+}
