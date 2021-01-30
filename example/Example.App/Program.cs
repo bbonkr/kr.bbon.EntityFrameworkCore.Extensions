e2b375f9bbd3db953b0e8a33d300a3740d0d0699
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2021-01-30 12:45:24 +0900

    #2 Add example project

---
+++ example/Example.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set;}
+
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
