b2451ce6261f62ec67803992a53c9b9f40d818df
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-06-20 13:36:48 +0900

    Add comments #17

--- src/kr.bbon.EntityFrameworkCore.Extensions/PagedModelExtensions.cs
+++ src/kr.bbon.EntityFrameworkCore.Extensions/PagedModelExtensions.cs
@@ -11,31 +11,70 @@ namespace kr.bbon.EntityFrameworkCore.Extensions
 {
     public interface IPagedModel<TModel>
     {
+        /// <summary>
+        /// Current page
+        /// </summary>
         int CurrentPage { get; init; }
+
+        /// <summary>
+        /// Records
+        /// </summary>
         IEnumerable<TModel> Items { get; init; }
+
+        /// <summary>
+        /// Items count per page
+        /// </summary>
         int Limit { get; init; }
+
+        /// <summary>
+        /// Total items count
+        /// </summary>
         int TotalItems { get; init; }
+
+        /// <summary>
+        /// Total page count
+        /// </summary>
         int TotalPages { get; init; }
     }
 
     public class PagedModel<TModel> : IPagedModel<TModel>
     {
+        /// <inheritdoc />
         public int CurrentPage { get; init; }
 
+        /// <inheritdoc />
         public int Limit { get; init; }
 
+        /// <inheritdoc />
         public int TotalItems { get; init; }
 
+        /// <inheritdoc />
         public int TotalPages { get; init; }
 
+        /// <inheritdoc />
         public IEnumerable<TModel> Items { get; init; }
     }
 
     public static class PagedModelExtensions
     {
+        /// <summary>
+        /// Default page value
+        /// </summary>
         public const int PAGE = 1;
+        /// <summary>
+        /// Default limit value
+        /// </summary>
         public const int LIMIT = 10;
 
+        /// <summary>
+        /// Get <see cref="IPagedModel{TModel}"/> instance of query result
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="page"></param>
+        /// <param name="limit"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<IPagedModel<TModel>> ToPagedModelAsync<TModel>(
             this IQueryable<TModel> query, 
             int page = PAGE, 
@@ -65,6 +104,15 @@ namespace kr.bbon.EntityFrameworkCore.Extensions
             };
         }
 
+        /// <summary>
+        /// Get <see cref="IPagedModel{TModel}"/> instance of query result
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="page"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
         public static TResult ToPagedModel<TModel, TResult>(
             this IQueryable<TModel> query, 
             int page = PAGE, 
